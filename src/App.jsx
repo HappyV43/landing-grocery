f8e793a3bdfa5645b04bde6e46d6680862fe21b5
Your Name <infilol123@gmail.com>
2024-10-16 00:07:04 +0700

    feat: commit ke dua

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Hero from "./components/hero";
 import Products from "./components/Products";
 import Testimonials from "./components/Testimonials";
 import Promo from "./components/Promo";
 import About from "./components/About";
+import Hero from "./components/Hero.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,36 @@
-import React from "react";
-
 const Navbar = () => {
   return (
-    <nav className="bg-green-600 text-white p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <a href="">
-          <h1 className="text-2xl font-bold">GroceryLand</h1>
+    <nav className="bg-green-200 shadow-md relative">
+      <div className="container mx-auto flex items-center justify-between py-4">
+        {/* Logo dan nama */}
+        <a href="/" className="flex items-center relative z-10">
+          <img
+            src="src/assets/Logo.png"
+            alt="logo"
+            className="w-32 h-30" // Ukuran logo yang lebih besar
+          />
+          <h1 className="text-3xl font-bold text-green-600 ml-2">
+            BelanjaMudah
+          </h1>
         </a>
-        <ul className="flex space-x-6">
-          <li>
-            <a href="#about" className="hover:text-gray-300">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="#features" className="hover:text-gray-300">
-              Features
-            </a>
-          </li>
-          <li>
-            <a href="#contact" className="hover:text-gray-300">
-              Contact
-            </a>
-          </li>
-        </ul>
+        {/* Menu Navbar */}
+        <div className="hidden md:flex space-x-4">
+          <a href="#products" className="text-gray-700 hover:text-green-500">
+            Products
+          </a>
+          <a href="#about" className="text-gray-700 hover:text-green-500">
+            About Us
+          </a>
+          <a
+            href="#testimonials"
+            className="text-gray-700 hover:text-green-500"
+          >
+            Testimonials
+          </a>
+          <a href="#contact" className="text-gray-700 hover:text-green-500">
+            Contact
+          </a>
+        </div>
       </div>
     </nav>
   );
